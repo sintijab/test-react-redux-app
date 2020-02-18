7cdecdce52db6dc7b05ec8f94adf45b1f33bd7dc
cynthiabirgele <sintija_birgele@inbox.lv>
2020-02-18 21:43:47 +0100

    Configure Webpack, define project structure

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: "./build",
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".js", ".jsx"]
+        },
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Test react-redux app',
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+  ]
+};
